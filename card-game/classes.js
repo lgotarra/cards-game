05a438c0013f79fafe45218efa9dd81e188b041d
lgotarra <97gotarra@gmail.com>
2020-04-05 17:41:00 +0200

    Created Hand class
    Merged Card constructors
    Added exceptions control to avoid bad parameters.

--- card-game/classes.js
+++ card-game/classes.js
@@ -1,13 +1,11 @@
 // Classes for the card game model.
 
 class Card {
-  constructor(value, family) {
-    // Default constructor
-    this.value = value;
-    this.family = family;
-  }
   constructor(value, family, display) {
     // Card with image
+    if (isNaN(value)) {
+      throw new TypeError(`"value" must be a number`);
+    }
     this.value = value;
     this.family = family;
     this.display = display;
@@ -24,4 +22,29 @@ class Card {
   }
 }
 
-
+class Hand {
+  constructor(list, kind, lastCard) {
+    // Default constructor
+    this.list = list;
+    switch (kind) {
+      case `player`:
+        this.visible = [];
+        break;
+      case `table`:
+        this.visible = Array.from(Array(list.length).keys());
+        break;
+      case `pile`:
+        if (lastCard) {
+          this.visible = [list.length - 1];
+        } else if (!lastCard) {
+          this.visible = [];
+        } else {
+          throw new TypeError(`"lastCard" must be boolean`);
+        }
+        break;
+      default:
+        throw new TypeError(`"kind" must be "player", "table" or "pile"`);
+    }
+    this.kind = kind;
+  }
+}
